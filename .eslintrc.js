564e09b7a4c32fa7714ed955941141f7f7ba8e24
Mustafa Aytunç <mustafaaytu@gmail.com>
2023-12-24 11:21:27 +0300

    WINGE-005 fix typo

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
